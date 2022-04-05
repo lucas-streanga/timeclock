1a1ab5b978188ac8f6db2f4e025dcac0dd8dadf4
BuildTools <unconfigured@null.spigotmc.org>
2022-04-05 17:10:19 -0500

    Changed create account functionality to no longer require an ID

--- create_account.php
+++ create_account.php
@@ -20,47 +20,49 @@ if(!$conn)
 else
 {
 	$username = filter_input(INPUT_POST, 'username');
-	$id = filter_input(INPUT_POST, 'id');
 
-	if($id != "" && $username != "" && is_numeric($id))
+	if($username != "")
 	{
 		//We need to use parameterized arguments for safety
 		//Check and see if an account with the ID already exists
-    	$query = $conn->prepare("SELECT * FROM account WHERE id=:id");
-		$query->bindParam(':id', $id);
+    	$query = $conn->prepare("SELECT * FROM account WHERE username=:username");
+		$query->bindParam(':username', $username);
 		$query->execute();
 		$rows = $query->fetchall();
 
     	if(count($rows) == 0)
     	{
-        	//Perfect, no account with this ID so we will create it!
-			$query = $conn->prepare("INSERT INTO account VALUES (:username, :id);");
+        	//Perfect, no account with this username  so we will create it!
+			$query = $conn->prepare("INSERT INTO account VALUES (:username, NULL);");
 			$query->bindParam(':username', $username);
-			$query->bindParam(':id', $id);
 			$success = true;
+			$rows = null;
 			try
 			{
 				$query->execute(); 
+				$query = $conn->prepare("SELECT id from account WHERE username=:username;");
+				$query->bindParam(':username', $username);
+				$query->execute();
+				$rows = $query->fetchall(PDO::FETCH_ASSOC);
 			}
 			catch(PDOException $e) 
 			{
 				echo "<p> <font color=red size='4pt'>Unable to create account: </font>". "<br>". $e->getMessage(). "</p>";
 				$success = false;
     		}
+			$id = $rows[0]["id"];
 			
 			if($success)
-				echo "<p> <font color=green size='4pt'>". 'Success! Created account with username "'. $username. '" and ID '. $id. '.'. "</font> </p>";
+				echo "<p> <font color=green size='4pt'>". 'Success! Created account with username "'. $username. '" with user ID '. $id. '.'. "</font> </p>";
     	}
 		else
 		{
 			//Uh oh, account already exists!
-        	echo "<p> <font color=red size='4pt'>". "Account with ID ". $id. " already exists!". "</font> </p>";
+        	echo "<p> <font color=red size='4pt'>". "Account with username ". $username. " already exists!". "</font> </p>";
 		}
 	}
-	if($id == "" || $username == "")
-		echo "<p> <font color=red size='4pt'>Username and ID must not be blank.</font> </p>";
-	if(is_numeric($id) == false)
-		echo "<p> <font color=red size='4pt' align='center'>ID must be an integer number.</font> </p>";
+	if($username == "")
+		echo "<p> <font color=red size='4pt'>Username must not be blank.</font> </p>";
 }
 
 }
