744cd0a82b9bc2961565914bb74e235a2a9f1fa3
michael.stokes <cse3311.team3@gmail.com>
2022-04-08 18:53:13 -0500

    Fixed (maybe) the sql

--- reports.php
+++ reports.php
@@ -13,6 +13,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 //Establish connection to the DB
 $conn = db_connect("timeclock");
 
+$userid = $_SESSION["userid"];
+
 // Check the connection
 if(!$conn)
 	die("Connection to database failed!");
@@ -24,23 +26,27 @@ else
 	
 	if(isset($_POST['simple_report_submit']))
 	{
-		if (!$result) {
-		    echo "Error executing query: " . mysql_error();
-		    exit;
-		}
-
-		if (mysql_num_rows($result) == 0) {
-		    echo "Empty result set";
-		    exit;
-		}
-		$sql = "SELECT c.Employee_Name, a.Employee_Id AS User, b.Task_Id_WP AS Task, (b.Clock_out-b.Clock_in) AS Time
+		$query = $conn->prepare("SELECT c.Employee_Name, a.Employee_Id AS User, b.Task_Id_WP AS Task, (b.Clock_out-b.Clock_in) AS Time
 			FROM   TC_User a
 			JOIN   Working_Period b ON a.Employee_Id = b.Employee_Id
             JOIN   TC_User c ON a.Employee_Id = c.Employee_Id
 			WHERE  b.Clock_out BETWEEN NOW()-INTERVAL 1 WEEK AND NOW()
-			GROUP BY Task";
+			AND a.Employee_Id = :userid
+			GROUP BY Task");
+		$query->bindParam(':userid', $userid);
+		$query->execute();
+		$rows = $query->fetchall();
+		if (!$rows) {
+		    echo "Error executing query: " . mysql_error();
+		    exit;
+		}
 
-		$result = mysql_query($sql);
+		if ($rows == 0) {
+		    echo "Empty result set";
+		    exit;
+		}
+		
+		echo $rows;
 		
 		echo "This is where the report for the last week would go :) <br>";
 		echo "Also the navbar is gone on this page, so we can get a pretty page to print";
