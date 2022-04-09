49b53d7a990e0078df923d68de279ff0cad0cc87
lucas-streanga <streangalucas@gmail.com>
2022-04-08 23:34:55 -0500

    Fixed reports.php (maybe, and just the code not the sql that scares me)

--- reports.php
+++ reports.php
@@ -8,7 +8,7 @@ include "include/check_login.php";
 check_login_or_redirect();
 
 //Check if any form was submitted, then run this code
-if($_SERVER["REQUEST_METHOD"] == "POST"){ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 //Establish connection to the DB
 $conn = db_connect("timeclock");
@@ -23,40 +23,48 @@ else
 	//The report view html file includes a print button
 	//You can output whatever you want to the screen and it will get printed on the button press
 	include "include/report_view.html";
-	
+
 	if(isset($_POST['simple_report_submit']))
 	{
-		$query = $conn->prepare("SELECT c.Employee_Name, a.Employee_Id AS User, b.Task_Id_WP AS Task, (b.Clock_out-b.Clock_in) AS Time
+		$success = true;
+		$sql = "SELECT c.Employee_Name, a.Employee_Id AS User, b.Task_Id_WP AS Task, (b.Clock_out-b.Clock_in) AS Time
 			FROM   TC_User a
 			JOIN   Working_Period b ON a.Employee_Id = b.Employee_Id
             JOIN   TC_User c ON a.Employee_Id = c.Employee_Id
 			WHERE  b.Clock_out BETWEEN NOW()-INTERVAL 1 WEEK AND NOW()
 			AND a.Employee_Id = :userid
-			GROUP BY Task");
+			GROUP BY Task";
+
+		$query = $conn->prepare(sql);
 		$query->bindParam(':userid', $userid);
-		$query->execute();
-		$rows = $query->fetchall();
-		if (!$rows) {
-		    echo "Error executing query: " . mysql_error();
-		    exit;
+		try
+		{
+			$query->execute();
+			//Use PDO:FETCH_ASSOC... mysql_fetch_assoc isn't supported...
+			$rows = $query->fetchall(PDO:FETCH_ASSOC);
+			//PDO is exception based - the rows will be empty if nothing is there
+			//If the query fails, you will get an exception! Not an empty result
+		}
+		//Throwable will catch everything, Exception will miss somethings...
+		catch(Throwable $e)
+		{
+			echo "<p> <font color=red size='4pt'>Unable to fetch report: </font>". "<br>". $e->getMessage(). "</p>";
+			$success = false;
 		}
 
-		if ($rows == 0) {
+		//count will give the correct eval
+		if (count($rows) == 0)
+		{
 		    echo "Empty result set";
 		    exit;
 		}
-		
-		echo $rows;
-		
+
+		//We have a function for a table.
+		if($success)
+			echo html_table($rows);
+
 		echo "This is where the report for the last week would go :) <br>";
 		echo "Also the navbar is gone on this page, so we can get a pretty page to print";
-		while ($row = mysql_fetch_assoc($result)) {
-				echo $row["Username"];
-    			echo $row["User"];
-    			echo $row["Task"];
-   		        echo $row["Time"];
-		}
-		mysql_free_result($result);
 		//We can run queries and generate the reports here, output them to html easily
 		//The report_gen.php file will include functions for generating the reports.
 		//Right now, only html_table() is included, to generate an html table from a db select.
@@ -72,4 +80,3 @@ else
 	include "include/reports.html";
 }
 ?>
-
