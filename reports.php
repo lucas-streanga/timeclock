9a7ea4df8d5fe6c85edc4b854fc9116805aa36d3
lucas-streanga <streangalucas@gmail.com>
2022-04-09 05:14:52 -0500

    i

--- reports.php
+++ reports.php
@@ -105,6 +105,83 @@ ORDER BY DAY(clock_in)
 			//PDO is exception based - the rows will be empty if nothing is there
 			//If the query fails, you will get an exception! Not an empty result
 		}*/
+		$overall_sql = '
+		SELECT  DAYNAME(clock_in) as Day,
+		task_name as Task,
+		clock_in as "In",
+		clockout as "Out",
+		TIMEDIFF(clockout, clock_in) as Time
+		FROM working_period
+		WHERE user_id=:userid AND clockout BETWEEN NOW()-INTERVAL 1 WEEK AND NOW()
+		GROUP BY DAYNAME(clock_in)
+		ORDER BY DAY(clock_in);';
+
+		$totals_per_task_sql = '
+		SELECT task_name as Task,
+		total_seconds_to_time(SUM(TIME_TO_SEC(TIMEDIFF(clockout, clock_in))))
+		as "Total (HH:MM:SS)"
+	 	FROM working_period
+	 	WHERE user_id=3
+	 	GROUP BY task_name
+		ORDER BY DAY(clock_in);';
+
+		$totals_per_day_sql = '
+		SELECT DAYNAME(clock_in) as Day,
+		total_seconds_to_time(SUM(TIME_TO_SEC(TIMEDIFF(clockout, clock_in))))
+		as "Total (HH:MM:SS)"
+		FROM working_period
+	 	WHERE user_id=:userid AND clockout BETWEEN NOW()-INTERVAL 1 WEEK AND NOW()
+	 	GROUP BY DAYNAME(clock_in)
+	  ORDER BY DAY(clock_in);';
+
+		$report_total_sql = '
+		SELECT total_seconds_to_time(SUM(TIME_TO_SEC(TIMEDIFF(clockout, clock_in))))
+		as "Report Total (HH:MM:SS)"
+ 		FROM working_period
+ 		WHERE user_id=:userid;';
+
+		$html_ret = "";
+
+		//Don't bother catching exceptions here, we'll do that when we call this function...
+
+		$query = $conn->prepare($overall_sql);
+		$query->bindParam(':userid', $userid);
+		$query->execute();
+		$rows = $query->fetchall(PDO::FETCH_ASSOC);
+		//Uh oh! Nothing to show...
+		if(count($rows) || !rows)
+		{
+			return null;
+		}
+		$html_ret .= html_table($rows) . "<br>";
+
+		$query = $conn->prepare($totals_per_task_sql);
+		$query->bindParam(':userid', $userid);
+		$query->execute();
+		$rows = $query->fetchall(PDO::FETCH_ASSOC);
+		//Uh oh! Nothing to show...
+		if($rows && count($rows) != 0)
+			$html_ret .= html_table($rows) . "<br";
+
+		$query = $conn->prepare($totals_per_day_sql);
+		$query->bindParam(':userid', $userid);
+		$query->execute();
+		$rows = $query->fetchall(PDO::FETCH_ASSOC);
+		//Uh oh! Nothing to show...
+		if($rows && count($rows) != 0)
+			$html_ret .= html_table($rows) . "<br";
+
+		$query = $conn->prepare($report_total_sql);
+		$query->bindParam(':userid', $userid);
+		$query->execute();
+		$rows = $query->fetchall(PDO::FETCH_ASSOC);
+		//Uh oh! Nothing to show...
+		if($rows && count($rows) != 0)
+			$html_ret .= html_table($rows);
+
+		print $html_ret;
+
+		return $html_ret;
 		$html = null;
 		try
 		{
