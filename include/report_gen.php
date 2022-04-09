5f79c0c286174d335dff79197fb07505493ee09e
lucas-streanga <streangalucas@gmail.com>
2022-04-09 05:53:30 -0500

    i

--- include/report_gen.php
+++ include/report_gen.php
@@ -6,7 +6,7 @@ function html_table($rows)
 {
 	$ret = "";
 	$table_headers = $rows[0];
-	$ret = $ret . "<table style='font-size: 13px;'>";
+	$ret = $ret . "<table>";
 	foreach(array_keys($table_headers) as &$column_name)
 		$ret = $ret . "<th>" . $column_name . "</th>";
 
