55fa754325377722e2aabf1e1d1062dd6f2ccac5
lucas-streanga <streangalucas@gmail.com>
2022-04-11 00:22:06 -0500

    Home changes

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php @session_start(); ?>
 <!DOCTYPE html><html><head><title>Home</title></head>
 <?php
-include "include/navbar.html";
+//Check if the user is logged in
+//If not, only show them the home and login navbar...
+
 include "include/check_login.php";
-check_login_or_redirect();
+
+if(check_login())
+	include "include/navbar.html";
+else
+{
+	echo "
+	<html>
+		<div id='navbar'>
+	 		<a href='./login.php'>Login</a>
+		</div>
+	</html>";
+}
 ?>
 
 <div align="center">
