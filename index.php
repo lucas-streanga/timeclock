afe7f69b0b13ba9ef23434cc62729347715c488e
lucas-streanga <streangalucas@gmail.com>
2022-04-11 00:26:22 -0500

    Home page changes

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ else
 	echo "
 	<html>
 		<div id='navbar'>
+			<a href='./index.php'>Home</a
 	 		<a href='./login.php'>Login</a>
 		</div>
 	</html>";
@@ -20,7 +21,15 @@ else
 ?>
 
 <div align="center">
+	<br>
 	<h1>Welcome to Timeclock!</h1>
+	<br>
+	<h2>
+		Timeclock is a web application for tracking hours. <br>
+		Create an account or login to start clocking time. <br>
+		You can categorize your time clocks by <b> task <b>.<br>
+		You can view and print reports of your time spent. <br>
+	</h2>
 </div>
 
 </html>
