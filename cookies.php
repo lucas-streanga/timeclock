19db7d70eb29a354fcab678d18a36ae85b95dcc5
lucas-streanga <streangalucas@gmail.com>
2022-04-11 01:09:30 -0500

    Added cookie policy

---
+++ cookies.php
@@ -0,0 +1,17 @@
+<?php include "include/navbar.html";?>
+
+<html><head><title>Cookie Policy</title></head>
+<div align="center">
+	<br>
+	<h1>Cookie Policy</h1>
+	<br>
+	<h2>
+		This site requires cookies to operate. <br><br>
+
+		One session cookie is used to remember your login information. <br>
+		This cookie never stores personal information. <br>
+		Your cookie is cleared whenever your browsing session ends (browser closed). <br><br>
+		You may disable cookies from your browser settings, but this site will become inoperable.
+	</h2>
+</div>
+</html>
