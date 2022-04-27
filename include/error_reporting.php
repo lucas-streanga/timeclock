4af1abc104857373b8a5a1b5e94a79e9635795af
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 02:38:36 -0500

    Fixed error reporting

--- include/error_reporting.php
+++ include/error_reporting.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(-1); // display all faires
-ini_set('display_errors', 1);  // ensure that faires will be seen
-ini_set('display_startup_errors', 1); // display faires that didn't born
+$report_errors = getenv('PHPDISPLAYERR');
+error_reporting($report_errors); // display all faires
+ini_set('display_errors', $report_errors);  
 ?>
